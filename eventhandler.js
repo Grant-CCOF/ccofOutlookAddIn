b4b2f4ce874bdcc4d49347e3461b45bc22d5cde8
Grant-CCOF <grant@ccofficefurniture.com>
2025-06-17 08:07:05 -0400

    Update eventhandler.js redirect

--- eventhandler.js
+++ eventhandler.js
@@ -12,7 +12,7 @@ const msalConfig = {
   auth: {
     clientId: "63e4f7da-45fc-45fa-9300-72b3038e72ef",
     authority: "https://login.microsoftonline.com/common",
-    redirectUri: "https://localhost:3000/"
+    redirectUri: "https://outlook.office.com/"
   },
   cache: {
     cacheLocation: "localStorage",
